a7aeb073f22b0c1a709aa7d33602577a829bcbd8
csolomita <c.solomita@gmail.com>
2018-02-17 17:38:19 -0800

    2/17

---
+++ ecard/Model/Greetings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecard.Model
+{
+    public class Greetings
+    {
+        public int? ID { get; set; }
+        public string friendname { get; set; }
+        public string friendemail { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+        public string sendername { get; set; }
+        public string senderemail { get; set; }
+    }
+}
